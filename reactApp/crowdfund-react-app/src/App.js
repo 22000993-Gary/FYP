7421c952df88136455fda3049973f0f6ed159660
22000993-Gary <22000993@myrp.edu.sg>
2024-05-02 14:14:22 +0800

    edits

--- reactApp/crowdfund-react-app/src/App.js
+++ reactApp/crowdfund-react-app/src/App.js
@@ -1,14 +1,16 @@
-
+import React from 'react';
 import './App.css';
+import Sidebar from './Sidebar.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        hello
-      </header>
+    <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
+      <div className="sm:flex hidden mr-10 relative">
+        <Sidebar />
+      </div>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
